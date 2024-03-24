7e61986750a459f935e0a5f4c20d735357ac8257
KeysiJones <keysijones02@gmail.com>
2024-03-24 14:32:21 -0300

    Updates frame gap and padding

--- src/components/PhoneFrame.jsx
+++ src/components/PhoneFrame.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 
 const PhoneFrame = ({ children }) => (
-  <div className='flex flex-col items-center self-center w-full h-full px-4 py-6 gap-y-4 sm:w-96 min-w-min bg-slate-100 sm:rounded-2xl sm:h-4/5'>
+  <div className='flex flex-col items-center self-center w-full h-full p-6 gap-y-4 sm:w-96 min-w-min bg-slate-100 sm:rounded-2xl sm:h-4/5'>
     <div>
-      <p className='text-xl font-bold'>Dashboard</p>
+      <p className='mt-4 text-xl font-bold'>Dashboard</p>
     </div>
     {children}
   </div>
